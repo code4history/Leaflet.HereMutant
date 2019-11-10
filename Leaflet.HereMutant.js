99a282b63b7e9554baed05dd9a92891f20228e6b
Kohei Otsuka <kochizufan@gmail.com>
2019-11-10 09:09:45 +0900

    Initial commit

---
+++ Leaflet.HereMutant.js
@@ -0,0 +1,225 @@
+// Leaflet.HereMutant - use HERE Map JS basemaps in Leaflet.
+// See https://github.com/code4history/Leaflet.HereMutant
+
+L.HereMutant = L.Layer.extend({
+	options: {
+		// serviceConfig option value, which is given to H.service.Platform initialization
+		servicesConfig: {},
+
+		// 🍂option opacity: Number = 1.0
+		// The opacity of the HereMutant
+		opacity: 1,
+
+		// 🍂option debugRectangle: Boolean = false
+		// Whether to add a rectangle with the bounds of the mutant to the map.
+		// Only meant for debugging, most useful at low zoom levels.
+		debugRectangle: true,
+	},
+
+	initialize: function(options) {
+		L.Util.setOptions(this, options);
+	},
+
+	onAdd: function(map) {
+		this._map = map;
+
+		this._initMutantContainer();
+
+		this._initMutant();
+
+		map.on("move zoom moveend zoomend", this._update, this);
+		map.on("resize", this._resize, this);
+		this._resize();
+	},
+
+	onRemove: function(map) {
+		map._container.removeChild(this._mutantContainer);
+		this._mutantContainer = undefined;
+		map.off("move zoom moveend zoomend", this._update, this);
+		map.off("resize", this._resize, this);
+		this._mutant.removeEventListener(
+			"mapviewchange",
+			this._mapviewChangeClosure
+		);
+		if (this._canvasOverlay) {
+			this._canvasOverlay.remove();
+		}
+	},
+
+	// Create the HTMLElement for the mutant map, and add it as a children
+	// of the Leaflet Map container
+	_initMutantContainer: function() {
+		if (!this._mutantContainer) {
+			this._mutantContainer = L.DomUtil.create(
+				"div",
+				"leaflet-here-mutant leaflet-top leaflet-left"
+			);
+			this._mutantContainer.id =
+				"_MutantContainer_" + L.Util.stamp(this._mutantContainer);
+			this._mutantContainer.style.zIndex = "200"; //leaflet map pane at 400, controls at 1000
+			this._mutantContainer.style.pointerEvents = "none";
+
+			this._map.getContainer().appendChild(this._mutantContainer);
+		}
+
+		this.setElementSize(this._mutantContainer, this._map.getSize());
+	},
+
+	// Create the mutant map inside the mutant container
+	_initMutant: function() {
+		if (!this._mutantContainer) return;
+
+		var platform = new H.service.Platform({
+			apikey: this.options.apikey,
+			servicesConfig: this.options.servicesConfig,
+		});
+		const defaultLayers = platform.createDefaultLayers();
+		var map = new H.Map(
+			this._mutantContainer,
+			defaultLayers.vector.normal.map,
+			{}
+		);
+
+		this._mutant = map;
+		var self = this;
+		this._mapviewChangeClosure = function(ev) {
+			self._onMapViewChange(ev);
+		};
+		map.addEventListener("mapviewchange", this._mapviewChangeClosure);
+
+		// 🍂event spawned
+		// Fired when the mutant has been created.
+		this.fire("spawned", { mapObject: map });
+
+		// Call _update once, so that it can fetch the mutant's canvas and
+		// create the L.ImageOverlay
+		L.Util.requestAnimFrame(this._update, this);
+	},
+
+	// Fetches the map's current *projected* (EPSG:3857) bounds, and returns
+	// an instance of H.geo.Rect
+	_leafletBoundsToHereRect: function() {
+		var bounds = this._map.getBounds();
+		return new H.geo.Rect(
+			bounds.getNorth(),
+			bounds.getWest(),
+			bounds.getSouth(),
+			bounds.getEast()
+		);
+	},
+
+	// Given an instance of H.geo.Rect, returns an instance of L.LatLngBounds
+	// This depends on the current map center, as to shift the bounds on
+	// multiples of 360 in order to prevent artifacts when crossing the
+	// antimeridian.
+	_hereRectToLeafletBounds: function() {
+		var rect = this._mutant
+			.getViewModel()
+			.getLookAtData()
+			.bounds.getBoundingBox();
+
+		return L.latLngBounds([
+			L.latLng(rect.getTop(), rect.getLeft()),
+			L.latLng(rect.getBottom(), rect.getRight()),
+		]);
+	},
+
+	_update: function() {
+		if (this._map && this._mutant) {
+			this._mutant.getViewModel().setLookAtData({
+				bounds: this._leafletBoundsToHereRect(),
+			});
+		}
+	},
+
+	_resize: function() {
+		var size = this._map.getSize();
+		if (
+			this._mutantContainer.style.width === size.x &&
+			this._mutantContainer.style.height === size.y
+		)
+			return;
+		this.setElementSize(this._mutantContainer, size);
+		if (!this._mutant) return;
+	},
+
+	_onMapViewChange: function(ev) {
+		if (!this._mutantCanvas) {
+			this._mutantCanvas = this._mutantContainer.querySelector("canvas");
+		}
+
+		if (this._map && this._mutantCanvas) {
+			// Despite the event name and this method's name, fetch the mutant's
+			// visible MapRect, not the mutant's region. It uses projected
+			// coordinates (i.e. scaled EPSG:3957 coordinates). This prevents
+			// latitude shift artifacts.
+			var bounds = this._hereRectToLeafletBounds();
+
+			// The mutant will take one frame to re-stitch its tiles, so
+			// repositioning the mutant's overlay has to take place one frame
+			// after the 'region-change-end' event, in order to avoid graphical
+			// glitching.
+
+			L.Util.cancelAnimFrame(this._requestedFrame);
+
+			this._requestedFrame = L.Util.requestAnimFrame(function() {
+				if (!this._canvasOverlay) {
+					this._canvasOverlay = L.imageOverlay(null, bounds);
+
+					// Hack the ImageOverlay's _image property so that it doesn't
+					// create a HTMLImageElement
+					var img = (this._canvasOverlay._image = L.DomUtil.create("div"));
+
+					L.DomUtil.addClass(img, "leaflet-image-layer");
+					L.DomUtil.addClass(img, "leaflet-zoom-animated");
+
+					// Move the mutant's canvas out of its container, and into
+					// the L.ImageOverlay's _image
+					this._mutantCanvas.parentElement.removeChild(this._mutantCanvas);
+					img.appendChild(this._mutantCanvas);
+
+					this._canvasOverlay.addTo(this._map);
+					this._updateOpacity();
+				} else {
+					this._canvasOverlay.setBounds(bounds);
+				}
+				this._mutantCanvas.style.width = "100%";
+				this._mutantCanvas.style.height = "100%";
+				this._mutantCanvas.style.position = "absolute";
+
+				if (this.options.debugRectangle) {
+					if (!this.rectangle) {
+						this.rectangle = L.rectangle(bounds, {
+							fill: false,
+						}).addTo(this._map);
+					} else {
+						this.rectangle.setBounds(bounds);
+					}
+				}
+			}, this);
+		}
+	},
+
+	// 🍂method setOpacity(opacity: Number): this
+	// Sets the opacity of the HereMutant.
+	setOpacity: function(opacity) {
+		this.options.opacity = opacity;
+		this._updateOpacity();
+		return this;
+	},
+
+	_updateOpacity: function() {
+		if (this._mutantCanvas) {
+			L.DomUtil.setOpacity(this._mutantCanvas, this.options.opacity);
+		}
+	},
+
+	setElementSize: function(e, size) {
+		e.style.width = size.x + "px";
+		e.style.height = size.y + "px";
+	},
+});
+
+L.hereMutant = function hereMutant(options) {
+	return new L.HereMutant(options);
+};
---
+++ README.md
@@ -0,0 +1,39 @@
+# Leaflet.HereMutant
+
+A [LeafletJS](http://leafletjs.com/) plugin to use [HERE Map JS](https://developer.here.com/documentation/maps/dev_guide/topics/overview.html) basemaps.
+
+The name comes from [MapkitMutant](https://gitlab.com/IvanSanchez/Leaflet.GridLayer.GoogleMutant). It's catchy, even if MapkitMutant doesn't use DOM mutation observers.
+
+## Usage
+
+Include the HERE Map JS API in your HTML, plus Leaflet:
+
+```html
+<link rel="stylesheet" href="https://unpkg.com/leaflet@1.3.3/dist/leaflet.css" />
+<script src="https://unpkg.com/leaflet@1.3.3/dist/leaflet.js"></script>
+<script src="https://js.api.here.com/v3/3.1/mapsjs-core.js" type="text/javascript" charset="utf-8"></script>
+<script src="https://js.api.here.com/v3/3.1/mapsjs-service.js" type="text/javascript" charset="utf-8"></script>
+```
+
+Include the HereMutant javascript file:
+
+```html
+<script src='https://unpkg.com/leaflet.heremutant@latest/Leaflet.HereMutant.js'></script>
+```
+
+Then, you can create an instance of `L.HereMutant` on your JS code:
+
+```javascript
+var roads = L.hereMutant({
+	// API Key which can be got at 'https://developer.here.com/'
+	apikey: 'xxxx',
+
+    // Optional: serviceConfig option value, which is given to H.service.Platform initialization
+	servicesConfig: {}
+}).addTo(map);
+```
+
+## Legalese
+
+Licensed under MIT. See the LICENSE file for details.
+
